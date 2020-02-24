56788528dffa3205624cba31f7055d6ade6507db
Nitin Londey <nitinlondey@Nitins-MacBook-Pro.local>
2020-02-24 16:48:17 -0500

    some https

--- src/main/java/WebSecurityConfig.java
+++ src/main/java/WebSecurityConfig.java
@@ -1,13 +1,14 @@
-/*
- * import org.springframework.context.annotation.Configuration; import
- * org.springframework.security.config.annotation.web.builders.HttpSecurity;
- * import org.springframework.security.config.annotation.web.configuration.
- * WebSecurityConfigurerAdapter;
- * 
- * @Configuration public class WebSecurityConfig extends
- * WebSecurityConfigurerAdapter {
- * 
- * @Override protected void configure(HttpSecurity http) throws Exception {
- * http.csrf().disable(); http.requiresChannel().requestMatchers(r ->
- * r.getHeader("X-Forwarded-Proto") != null).requiresSecure(); } }
- */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+	}
+}
